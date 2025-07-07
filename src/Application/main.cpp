4b0bdcf2492daf724fc2af184539680fa9d264a5
靳晓丹 <1015903494@qq.com>
2025-07-07 12:30:37 +0800

    修改cmakelist，让所有子项目都能使用assimp，fmt，spdlog，eigen库

--- src/Application/main.cpp
+++ src/Application/main.cpp
@@ -1,7 +1,7 @@
 #include <QApplication>
 #include <QScreen>
-//#include <spdlog/spdlog.h>
-//#include <fmt/chrono.h>
+#include <spdlog/spdlog.h>
+#include <fmt/chrono.h>
 #ifdef Q_OS_WIN
 #include <Windows.h>
 #endif
@@ -25,22 +25,22 @@ int main(int argc, char *argv[])
 	QApplication a(argc, argv);
 	eApp->init();
 
-//	spdlog::set_pattern("[%n] [%^%l%$] %v");
-//#ifdef DEBUG
-//	spdlog::set_level(spdlog::level::debug);
-//#else
-//	spdlog::set_level(spdlog::level::info);
-//#endif
-//	spdlog::set_default_logger(get_logger("Default"));
-//
-//	// Log the start time point.
-//	// Here we do not use std::chrono::system_clock, because the formatter
-//	// of std::chrono::time_point in fmtlib 9.1.0 formats it to local time,
-//	// but the formatter in C++20's std::format formats it to UTC time.
-//	// In the future we may migrate the whole project to C++20/23, so we do
-//	// not want to use an inconsistent feature.
-//	std::tm now = fmt::localtime(std::time(nullptr));
-//	spdlog::info("Dandelion 3D, started at {:%Y-%m-%d %H:%M:%S%z}", now);
+	spdlog::set_pattern("[%n] [%^%l%$] %v");
+#ifdef DEBUG
+	spdlog::set_level(spdlog::level::debug);
+#else
+	spdlog::set_level(spdlog::level::info);
+#endif
+	//spdlog::set_default_logger(get_logger("Default"));
+
+	// Log the start time point.
+	// Here we do not use std::chrono::system_clock, because the formatter
+	// of std::chrono::time_point in fmtlib 9.1.0 formats it to local time,
+	// but the formatter in C++20's std::format formats it to UTC time.
+	// In the future we may migrate the whole project to C++20/23, so we do
+	// not want to use an inconsistent feature.
+	std::tm now = fmt::localtime(std::time(nullptr));
+	spdlog::info("Dandelion 3D, started at {:%Y-%m-%d %H:%M:%S%z}", now);
 
     MainWindow w;
     w.show();
