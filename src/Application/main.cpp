4cffddea6293c957d3a0c32758fcf2973cfd18da
靳晓丹 <1015903494@qq.com>
2025-08-24 23:07:37 +0800

    初始化OpenGL版本，并设置Qt的高dpi支持

--- src/Application/main.cpp
+++ src/Application/main.cpp
@@ -1,10 +1,8 @@
 #include <QApplication>
-#include <QScreen>
+#include <QSurfaceFormat>
+#include <QOpenGLContext>
 #include <spdlog/spdlog.h>
 #include <fmt/chrono.h>
-#ifdef Q_OS_WIN
-#include <Windows.h>
-#endif
 
 #include "MainWindow.h"
 #include "ElaApplication.h"
@@ -12,6 +10,7 @@
 
 int main(int argc, char *argv[])
 {
+	// 设置不同 Qt 版本的高DPI支持
 #if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
 	QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
@@ -36,7 +35,26 @@ int main(int argc, char *argv[])
 	std::tm now = fmt::localtime(std::time(nullptr));
 	spdlog::info("Dandelion 3D, started at {:%Y-%m-%d %H:%M:%S%z}", now);
 
+	// 设置OpenGL版本
+	QSurfaceFormat fmt;
+	fmt.setDepthBufferSize(24);
+	if (QOpenGLContext::openGLModuleType() == QOpenGLContext::LibGL) {
+		// 桌面 OpenGL（Core Profile）
+		qDebug("Requesting 3.3 core context");
+		fmt.setVersion(3, 3);
+		fmt.setProfile(QSurfaceFormat::CoreProfile);
+		fmt.setOption(QSurfaceFormat::DeprecatedFunctions, false); // 禁用旧函数
+	}
+	else {
+		// OpenGL ES
+		qDebug("Requesting 3.0 context");
+		fmt.setVersion(3, 0);
+		fmt.setRenderableType(QSurfaceFormat::OpenGLES); // 显式声明为 ES
+	}
+	QSurfaceFormat::setDefaultFormat(fmt);
+
     MainWindow w;
     w.show();
+
     return a.exec();
 }
